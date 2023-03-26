bd3265f0352f285044cbdd47ef67f77180cbf669
Scenaristeur <scenaristeur@gmail.com>
2023-03-26 10:30:25 +0200

    select room

---
+++ dist/js/about.8e1437f2.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunknoosphere3"]=self["webpackChunknoosphere3"]||[]).push([[443],{9468:function(t,e,r){r.r(e),r.d(e,{default:function(){return h}});var s=r(3396);const n={class:"about"},a=(0,s._)("a",{href:"https://github.com/scenaristeur/tiptap-yjs",target:"_blank"},"https://github.com/scenaristeur/tiptap-yjs",-1),u=[a];function c(t,e){return(0,s.wg)(),(0,s.iD)("div",n,u)}var i=r(89);const o={},p=(0,i.Z)(o,[["render",c]]);var h=p}}]);
+//# sourceMappingURL=about.8e1437f2.js.map
