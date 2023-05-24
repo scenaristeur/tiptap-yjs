eb4e03b92e6bb20c684e279e4372c395d206dd82
Scenaristeur <scenaristeur@gmail.com>
2023-05-24 15:32:22 +0200

    ^qques^ pr├®cisions^

--- js/about.b088d72d.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunknoosphere3"]=self["webpackChunknoosphere3"]||[]).push([[443],{6501:function(t,e,r){r.r(e),r.d(e,{default:function(){return h}});var s=r(3396);const n={class:"about"},a=(0,s._)("a",{href:"https://github.com/scenaristeur/tiptap-yjs",target:"_blank"},"https://github.com/scenaristeur/tiptap-yjs",-1),u=[a];function c(t,e){return(0,s.wg)(),(0,s.iD)("div",n,u)}var i=r(89);const o={},p=(0,i.Z)(o,[["render",c]]);var h=p}}]);
-//# sourceMappingURL=about.b088d72d.js.map
